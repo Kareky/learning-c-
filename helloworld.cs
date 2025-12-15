9de1b34050375c680104b8a5b07b02f3c72e8a4a
Kareky <riccardo.morosin@hotmail.com>
2025-12-15 17:13:13 +0100

    Hello World

---
+++ helloworld.cs
@@ -0,0 +1,8 @@
+using System;
+﻿namespace TourOfCsharp;
+class main {
+    static void Main() {
+        //Line to print Hello World to the console
+        Console.WriteLine("Hello, World!");
+    }
+}
