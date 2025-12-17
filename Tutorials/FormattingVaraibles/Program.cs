024f89753e0abfe9825fbdb71e75335c37d43237
Kareky <riccardo.morosin@hotmail.com>
2025-12-17 20:14:06 +0100

    Testing Currency Formatting

--- Tutorials/FormattingVaraibles/Program.cs
+++ Tutorials/FormattingVaraibles/Program.cs
@@ -1,4 +1,10 @@
+//Testing String Formatting
 string first = "Hello";
 string second = "World";
 Console.WriteLine("{1} {0}!", first, second);
 Console.WriteLine("{0} {0} {0}!", first, second);
+
+//Testing Currency Formatting
+decimal price = 123.45m;
+int discount = 50;
+Console.WriteLine($"Price: {price:C} (Save {discount:C})");
