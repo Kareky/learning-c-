8c54a1bfe45b6fefaf57307a2bcb7e45ef305dbb
Kareky <riccardo.morosin@hotmail.com>
2025-12-17 20:21:06 +0100

    Testing Padding

--- Tutorials/FormattingVaraibles/Program.cs
+++ Tutorials/FormattingVaraibles/Program.cs
@@ -12,3 +12,15 @@ Console.WriteLine($"Price: {price:C} (Save {discount:C})");
 //Testing Percentage Formatting
 decimal tax = .36785m;
 Console.WriteLine($"Tax rate: {tax:P2}");
+
+//Testing Padding
+string paymentId = "769C";
+ string payeeName = "Mr. Stephen Ortega";
+ string paymentAmount = "$5,000.00";
+
+ var formattedLine = paymentId.PadRight(6);
+ formattedLine += payeeName.PadRight(24);
+ formattedLine += paymentAmount.PadLeft(10);
+
+ Console.WriteLine("1234567890123456789012345678901234567890");
+ Console.WriteLine(formattedLine);
