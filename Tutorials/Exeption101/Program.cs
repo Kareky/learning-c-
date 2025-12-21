f27ff0cbd3ce2f5529e13951e94e39da2d294d4a
Kareky <riccardo.morosin@hotmail.com>
2025-12-21 17:15:05 +0100

    Exercise to test the Stack order

--- Tutorials/Exeption101/Program.cs
+++ Tutorials/Exeption101/Program.cs
@@ -1,4 +1,18 @@
-﻿try
+﻿/*
+    Update the Process1 method to catch the exception that's thrown in the WriteMessage method.
+    The Process1 method should print the following message to console when the exception is caught:
+    "Exception caught in Process1"
+
+    Don't change any code outside the Process1 method.
+
+    When you run your updated application, it should generate the following output:
+    "∞
+    Exception caught in Process1
+    Exit program"
+
+*/
+
+try
 {
     Process1();
 }
@@ -11,7 +25,14 @@ Console.WriteLine("Exit program");
 
 static void Process1()
 {
-    WriteMessage();
+    try
+    {
+        WriteMessage();
+    }
+    catch
+    {
+        Console.WriteLine("Exception caught in Process1");
+    }
 }
 
 static void WriteMessage()
